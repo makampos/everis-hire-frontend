27310ca11a0c825eec95cb081a9aff3da72c9b4a
MG7 <matheus.campos@edu.unifil.br>
2021-06-05 19:59:15 -0300

    feat: add logout feat

--- everis-hire/src/app/_services/account.service.ts
+++ everis-hire/src/app/_services/account.service.ts
@@ -4,6 +4,8 @@ import { ToastrService } from 'ngx-toastr';
 import { Observable, ReplaySubject } from 'rxjs';
 import {map} from 'rxjs/operators';
 import { environment } from 'src/environments/environment';
+import { SpinnerService } from '../core/spinner/spinner.service';
+import { ResponseVM } from '../_models/ResponseVM';
 import { User } from '../_models/User';
 
 
@@ -16,12 +18,14 @@ export class AccountService {
   private currentUserSource = new ReplaySubject<User>(1);
   currentUser$ = this.currentUserSource.asObservable();
 
-  constructor(private http: HttpClient, private toastr: ToastrService) { }
+  constructor(private http: HttpClient,
+              private toastr: ToastrService,
+              private loader: SpinnerService) { }
 
   login(user: User){
     return this.http.post(this.baseUrl + 'account/authenticate', user).pipe(
-      map((response: any) => {       
-         user = response;         
+      map((response: any) => {        
+         const user = response;
         if(user) {
           localStorage.setItem('user',JSON.stringify(user));
           this.currentUserSource.next(user);
@@ -29,6 +33,16 @@ export class AccountService {
         }
       })
     )
+  }
+
+  logout(){
+    localStorage.removeItem('user');
+    this.currentUserSource.next();
+    this.toastr.info('Logout');
+  }
+
+  setCurrentUser(user: User) {
+    this.currentUserSource.next(user);
   }
 
 }
