e9fca75014506a545a7cc7f938b936345927fb28
MG7 <matheus.campos@edu.unifil.br>
2021-06-05 20:02:17 -0300

    feat: add intial template and logic for job component

--- everis-hire/src/app/job/job.component.ts
+++ everis-hire/src/app/job/job.component.ts
@@ -1,4 +1,13 @@
 import { Component, OnInit } from '@angular/core';
+import { FormBuilder, Validators } from '@angular/forms';
+import { ToastrService } from 'ngx-toastr';
+import { IRecruiter } from '../_models/IRecruiter';
+import { ResponseVM } from '../_models/ResponseVM';
+import { RecruiterService } from '../_services/recruiter.service';
+import { IJob } from './model/IJob';
+import { Priority } from './model/Priority';
+import { JobService } from './service/job.service';
+
 
 
 
@@ -8,10 +17,65 @@ import { Component, OnInit } from '@angular/core';
   styleUrls: ['./job.component.scss']
 })
 export class JobComponent implements OnInit {
+  recruiters: any = []
 
-  constructor() { }
+  constructor(private fb: FormBuilder,
+              private jobService: JobService,
+              private recruiterService: RecruiterService,
+              private toastr: ToastrService) { }
 
   ngOnInit(): void {
+    this.getRecruiters()
   }
 
+  priorities: Priority[] = [
+    {value: 1 },
+    {value: 2 },
+    {value: 3, },   
+  ];
+
+  jobForm = this.fb.group({
+    everJob: [''],
+    leaderCenters: [''],
+    ltfOrPl: [''],
+    managerSp: [''],
+    community: [''],
+    squad: [''],
+    projectId: [''],
+    allocationType: [''],
+    openingDate: [''],
+    technology: [''],
+    yearsOfExperience: [''],
+    desiredDate: [''],
+    maximumSalary: [''],
+    recruiter: [''],
+    priority: [''],
+    priorityDate: [''],
+    status: [''],
+    justification: [''],
+  })
+
+  save(){
+    let job = <IJob>{}
+    Object.assign(job,this.jobForm.value);
+    console.log(job)
+    this.jobService.postJob(job).subscribe((response) => {
+      if(response.success) {
+        this.toastr.success('Registro efetuado com sucesso!')
+      }
+    }, error => {
+      this.toastr.error(error.message)    
+    })
+  }
+
+  getRecruiters() {
+    this.recruiterService.getAllRecruiter().subscribe((response:ResponseVM<IRecruiter[]>) => {
+      if(response){        
+        this.recruiters = response;
+        console.log(this.recruiters);
+      }
+    }, error => {
+      this.toastr.error("Não foi possível obter a lista de recrutadores", error)
+    });
+  }
 }
---
+++ everis-hire/src/app/job/model/IJob.ts
@@ -0,0 +1,21 @@
+export interface IJob {
+        JobId: string 
+        EverJob: number,       
+        LeaderCenters: string, 
+        LtfOrPl: string, 
+        ManagerSp: string, 
+        Community: string,
+        Squad: string, 
+        ProjectId: string, 
+        AllocationType: string, 
+        OpeningDate: Date,
+        Technology: string, 
+        YearsOfExperience: number, 
+        DesiredDate:Date, 
+        MaximumSalary: number, 
+        Recruiter: string, 
+        Priority: number, 
+        PriorityDate: Date, 
+        Status: string, 
+        Justification: string,
+}
