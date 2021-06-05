e1afc9cec1145c699ce43ba400ec06630ca0f086
MG7 <matheus.campos@edu.unifil.br>
2021-06-05 20:04:51 -0300

    feat: set current user

--- everis-hire/src/app/app.component.ts
+++ everis-hire/src/app/app.component.ts
@@ -1,10 +1,33 @@
-import { Component } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
+import { SpinnerService } from './core/spinner/spinner.service';
+import { User } from './_models/User';
+import { AccountService } from './_services/account.service';
 
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
   styleUrls: ['./app.component.scss']
 })
-export class AppComponent {
-  title = 'everis-hire';
+export class AppComponent implements OnInit {
+
+  loading$ = this.loader.loading$;
+  // user: any;
+  constructor(private loader: SpinnerService,
+              private accountService: AccountService) {
+
+  }
+  ngOnInit(): void {
+    this.setCurrentUser();
+  }
+
+
+
+  setCurrentUser() {
+    const user:User = JSON.parse(localStorage.getItem('user') || '{}');
+    this.accountService.setCurrentUser(user);
+  }
+
+  
+
+
 }
